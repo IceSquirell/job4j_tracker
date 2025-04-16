bbdd19e52c595074876bb1d399f04294df05f958
viktorzaluckij <zaluckii98@gmail.com>
2025-04-16 16:48:23 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,19 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +31,18 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void printOf() {
+        System.out.println(name + " " + id);
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        Item item1 = new Item("Ivan");
+        Item item2 = new Item("Ivan", 2);
+        item.printOf();
+        item1.printOf();
+        item2.printOf();
     }
 }
